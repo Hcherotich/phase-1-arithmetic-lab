590d9089877b9ee6ed121adc90bac9a028a0859e
Hcherotich <cherohellen14@gmail.com>
2023-12-27 17:12:00 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 8;
+let num2 = 8
+let num5= 2
+let multiply = num1 * num2 - num5 // 62
 
-// Write your code here
+const random = Math.ceil(Math.random()*100);
+let num3 =40;
+let num4 = 36;
+let mod = num3 % num4;
+const setofnumbers =[1,7,14,20,3]
+const max= Math.max(...setofnumbers)
